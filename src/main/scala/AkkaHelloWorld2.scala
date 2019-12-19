eb85d3bca1308feb4600ca9a74d99739c4227fc9
Ivan <ivan.skoryk@icloud.com>
2019-12-19 21:48:58 +0200

    Completed lab3.

---
+++ src/main/scala/AkkaHelloWorld2.scala
@@ -0,0 +1,16 @@
+
+import akka.actor._
+
+class MyHelloActor(myName: String) extends Actor {
+  def receive: PartialFunction[Any, Unit] = {
+    case "hello" => println("hello from %s".format(myName))
+    case _       => println("'huh?', said %s".format(myName))
+  }
+}
+
+object Main2 extends App {
+  val system = ActorSystem("HelloSystem")
+  val helloActor = system.actorOf(Props(new MyHelloActor("Fred")), name = "helloactor")
+  helloActor ! "hello"
+  helloActor ! "buenos dias"
+}
